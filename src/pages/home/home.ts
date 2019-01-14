7be8fa9b0276644268c3235815fa8a86867f9b11
krstfrai <krystof.raiskub@gmail.com>
2019-01-14 23:38:04 +0100

    Created Basic UI for HomePage, Implemented Toast Message

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Toast } from 'ionic-angular';
+
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private searchTerm: string = '';
+  private mediaType: string = 'music';
 
+  constructor(public navCtrl: NavController, private toastCtrl: ToastController) {
+
+  }
+
+  // 1. Get Input Data
+  sendData() {
+    if(this.searchTerm !== '' && this.mediaType !== '') {
+
+    } else {
+      this.presentToast();
+    }
+  }
+  // 2. Send Request to API
+
+  // 3. Get response and parse
+
+  // 4. Display Data
+
+
+  // Toast Message
+  presentToast() {
+    let toast = this.toastCtrl.create({
+      message: 'Please enter Search term and Media type',
+      duration: 3000,
+      position: 'bottom'
+    });
+
+    toast.present();
   }
 
 }
